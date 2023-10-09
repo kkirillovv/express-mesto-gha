81eed964ba4a9830a1aeb3b8dcf74e2da287e10c
Kirillov Kirill <admin@sopka.net>
2023-10-09 22:17:32 +0300

    Тринадцатый спринт, версия 10.7

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,13 @@ const { Promise } = require('mongoose')
 const User = require('../models/user')
 const { ValidationError, NotFoundError } = require('../errors')
 
-const getUsers = (req, res) => {
-  User.find({})
-    .then((users) => res.send({ data: users }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }))
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({})
+    res.status(200).send({ data: users })
+  } catch (err) {
+    res.status(500).send({ message: 'Ошибка по умолчанию' })
+  }
 }
 
 // eslint-disable-next-line consistent-return
@@ -38,38 +41,44 @@ const createUser = async (req, res) => {
   }
 }
 
-const editUserData = (req, res) => {
-  const { name, about } = req.body
-  User.findByIdAndUpdate(req.user._id, { name, about })
-    .then((user) => res.send(user))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(ValidationError.statusCode).send({ message: 'Переданы некорректные данные при создании карточки.' })
-        return
-      }
-      if (err.message === 'NotFoundError') {
-        res.status(NotFoundError.statusCode).send({ message: `Карточка с Id = ${req.user._id} не найдена` })
-        return
-      }
-      res.status(500).send({ message: 'Ошибка по умолчанию' })
-    })
+// eslint-disable-next-line consistent-return
+const editUserData = async (req, res) => {
+  try {
+    const { name, about } = req.body
+    const user = await User.findByIdAndUpdate(req.user._id, { name, about })
+    if (!user) {
+      return Promise.reject(new NotFoundError(`Карточка с Id = ${req.user._id} не найдена`))
+    }
+    res.status(200).send({ data: user })
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      return res.status(ValidationError.statusCode).send({ message: 'Переданы некорректные данные при создании карточки.' })
+    }
+    if (err.message === 'NotFoundError') {
+      return res.status(NotFoundError.statusCode).send({ message: `Карточка с Id = ${req.user._id} не найдена` })
+    }
+    res.status(500).send({ message: 'Ошибка по умолчанию' })
+  }
 }
 
-const editUserAvatar = (req, res) => {
-  const { avatar } = req.body
-  User.findByIdAndUpdate(req.user._id, { avatar })
-    .then((user) => res.send(user))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        res.status(ValidationError.statusCode).send({ message: 'Переданы некорректные данные при создании карточки.' })
-        return
-      }
-      if (err.message === 'NotFoundError') {
-        res.status(NotFoundError.statusCode).send({ message: `Карточка с Id = ${req.user._id} не найдена` })
-        return
-      }
-      res.status(500).send({ message: 'Ошибка по умолчанию' })
-    })
+// eslint-disable-next-line consistent-return
+const editUserAvatar = async (req, res) => {
+  try {
+    const { avatar } = req.body
+    const user = await User.findByIdAndUpdate(req.user._id, { avatar })
+    if (!user) {
+      return Promise.reject(new NotFoundError(`Карточка с Id = ${req.user._id} не найдена`))
+    }
+    res.status(200).send({ data: user })
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      return res.status(ValidationError.statusCode).send({ message: 'Переданы некорректные данные при создании карточки.' })
+    }
+    if (err.message === 'NotFoundError') {
+      return res.status(NotFoundError.statusCode).send({ message: `Карточка с Id = ${req.user._id} не найдена` })
+    }
+    res.status(500).send({ message: 'Ошибка по умолчанию' })
+  }
 }
 
 // eslint-disable-next-line object-curly-newline
