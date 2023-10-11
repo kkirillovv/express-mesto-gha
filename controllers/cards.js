2d235c2e0cf5e0f7cf7bfc20623b3993811ba4f7
Kirillov Kirill <admin@sopka.net>
2023-10-11 08:52:41 +0300

    Тринадцатый спринт, версия 10.33

--- controllers/cards.js
+++ controllers/cards.js
@@ -30,9 +30,12 @@ const deleteCardById = async (req, res) => {
   try {
     const { cardId } = req.params
     if (!mongoose.Types.ObjectId.isValid(cardId)) {
-      return res.status(404).send({ message: `Карточка с Id = ${req.user._id} не найдена` })
+      return res.status(400).send({ message: `Карточка с Id = ${req.user._id} не найдена` })
     }
     const card = await Card.findByIdAndDelete(cardId)
+    if (!card) {
+      return res.status(404).json({ message: `Карточка с Id = ${req.user._id} не существует` })
+    }
     res.status(200).send({ data: card, message: 'Карточка удалена' })
   } catch (err) {
     if (err.name === CastError.name) {
