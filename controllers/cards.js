468cdbf3dce67bde8c1bb60b9ba67470e575f776
Kirillov Kirill <admin@sopka.net>
2023-10-12 03:25:31 +0300

    Тринадцатый спринт, версия 10.62

--- controllers/cards.js
+++ controllers/cards.js
@@ -44,25 +44,14 @@ const deleteCardById = async (req, res) => {
   }
 }
 
-// eslint-disable-next-line consistent-return
 const likeCardById = async (req, res) => {
-  try {
-    const { cardId } = req.params
-    const card = await Card.findByIdAndUpdate(
-      cardId,
-      { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-      { new: true },
-    )
-    if (!card) {
-      return res.status(404).json({ message: `Карточка с Id = ${req.user._id} не существует` })
-    }
-    res.status(constants.HTTP_STATUS_OK).send({ data: card })
-  } catch (err) {
-    if (err.name === CastError.name) {
-      return res.status(constants.HTTP_STATUS_BAD_REQUEST).send({ message: isCastError })
-    }
-    res.status(constants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({ message: isDefaultServerError })
-  }
+  const func = (cardId) => Card.findByIdAndUpdate(
+    cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+  const errorMessage = `Id = ${req.user._id} карточки не существует`
+  handleErrors(req, res, func, errorMessage)
 }
 
 const dislikeCardById = async (req, res) => {
