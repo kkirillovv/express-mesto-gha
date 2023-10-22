e74d512cf91dc3bce1e0d6cd2bb34cb700e0bf9d
Kirillov Kirill <admin@sopka.net>
2023-10-23 00:43:14 +0300

    Четырнадцатый спринт, версия 11.49

--- utils/regexp.js
+++ utils/regexp.js
@@ -1,4 +1,5 @@
-const urlRegExp = /^(http|https):\/\/\S+\.\S+$/
+const urlRegExp = /^(http|https):\/\/(www\.)?\S+\.\S+$/
+// /https?:\/\/(www\.)?[\w-]+(\.\w+)+([/\w-._~:?#[\]@!$&'()*+,;=]+)?#?/;
 const emailRegExp = /^\S+@\S+\.\S+$/
 
 module.exports = { urlRegExp, emailRegExp }
