07cd6835fbd8f6763c5d22bb4c5b0abc5b0aeb78
Kirillov Kirill <admin@sopka.net>
2023-10-22 18:49:23 +0300

    Четырнадцатый спринт, версия 11.22

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,25 @@
 const router = require('express').Router()
+const { celebrate, Joi } = require('celebrate')
+
 const {
   getUsers, getUserById, getUserInfo, editUserData, editUserAvatar,
 } = require('../controllers/users')
 
 router.get('', getUsers)
-router.get('/:userId', getUserById)
 router.get('/me', getUserInfo)
-router.patch('/me', editUserData)
-router.patch('/me/avatar', editUserAvatar)
+router.get('/:userId', getUserById)
+
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), editUserData)
+
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().pattern(/^(http|https):\/\/[^ "]+$/),
+  }),
+}), editUserAvatar)
 
 module.exports = router
